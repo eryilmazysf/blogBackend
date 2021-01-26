61026a5d62ee75c7eaa37a72b85a30db859701c5
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-01-26 00:57:34 -0500

    settings

--- blog_lyfe/settings.py
+++ blog_lyfe/settings.py
@@ -135,8 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'build/static']
-STATIC_ROOT = BASE_DIR / 'static'
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+else:
+    STATIC_ROOT = BASE_DIR / "static"
+# STATICFILES_DIRS = [BASE_DIR / 'build/static']
+# STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
