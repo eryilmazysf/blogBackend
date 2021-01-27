82a4b8f7171d222ec913cc0342fc0192005b3796
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-01-27 18:16:01 -0500

    password

--- user/serializers.py
+++ user/serializers.py
@@ -14,21 +14,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(write_only=True, required=True, style={
-                                      'input_type': 'password'})
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'password2')
-        extra_kwargs = {'password': {'write_only': True},
-                        'password2': {'write_only': True}}
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError(
-                {"password": "Password fields didn't match."})
-
-        return attrs
 
     def create(self, validated_data):
         user = User.objects.create_user(
